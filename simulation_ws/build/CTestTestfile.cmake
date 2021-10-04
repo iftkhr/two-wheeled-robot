0509a701808f33f8aab9a7085ac66bf7015d80f6
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2021-10-04 20:39:11 +0530

    create workspace

---
+++ simulation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/src
+# Build directory: /media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ simulation_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
