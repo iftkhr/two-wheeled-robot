c2846d38a76450ea31e2ec12d8490fed168f2414
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2021-10-04 21:42:40 +0530

    add urdf and launch file for rviz

--- simulation_ws/build/catkin_generated/setup_cached.sh
+++ simulation_ws/build/catkin_generated/setup_cached.sh
@@ -8,6 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/src:$ROS_PACKAGE_PATH"
--- simulation_ws/build/cmake_install.cmake
+++ simulation_ws/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/build/gtest/cmake_install.cmake")
+  include("/media/iftkhr/Iftakhar/Robotics/ROS/two-wheeled-robot/simulation_ws/build/m2wr_description/cmake_install.cmake")
 
 endif()
 
